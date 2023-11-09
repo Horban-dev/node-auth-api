8fc407b55175136ea524be3ed1acbc4ca243005f
Illia <illia.horban@gmail.com>
2023-11-09 11:35:57 +0200

    routes middleware, datas validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
-import { inject, injectable } from 'inversify';
+import { injectable, inject } from 'inversify';
 import { BaseController } from '../common/base.controller';
 import { HTTPError } from '../errors/http-errors.class';
 import { ILogger } from '../logger/logger.interface';
-import { LoggerService } from '../logger/logger.sevice';
 import { TYPES } from '../types';
+import 'reflect-metadata';
+import { IUsersController } from './users.controller.interface';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
 import { User } from './user.entity';
-import { IUsersController } from './users.controller.interface';
 import { UserService } from './users.service';
+import { ValidateMiddleware } from '../common/validate.middleware';
 
 @injectable()
 export class UserController extends BaseController implements IUsersController {
@@ -19,14 +20,19 @@ export class UserController extends BaseController implements IUsersController {
 	) {
 		super(loggerService);
 		this.bindRoutes([
-			{ path: '/register', method: 'post', func: this.register },
+			{
+				path: '/register',
+				method: 'post',
+				func: this.register,
+				middlewares: [new ValidateMiddleware(UserRegisterDto)],
+			},
 			{ path: '/login', method: 'post', func: this.login },
 		]);
 	}
 
 	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
 		console.log(req.body);
-		next(new HTTPError(401, 'error', 'login'));
+		next(new HTTPError(401, 'ошибка авторизации', 'login'));
 	}
 
 	async register(
@@ -36,8 +42,8 @@ export class UserController extends BaseController implements IUsersController {
 	): Promise<void> {
 		const result = await this.userService.createUser(body);
 		if (!result) {
-			next(new HTTPError(422, 'Already registered'));
+			return next(new HTTPError(422, 'Такой пользователь уже существует'));
 		}
-		this.ok(res, result);
+		this.ok(res, { email: result.email });
 	}
 }
